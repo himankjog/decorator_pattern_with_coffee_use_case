df0fc7e1a797cecf6a394b8c1c61122f4380567b
Himank Jog <himankj@amazon.com>
2021-12-31 14:58:49 +0530

    Removed size data member from CondimentDecorator

--- src/main/java/decorators/condiment/CondimentDecorator.java
+++ src/main/java/decorators/condiment/CondimentDecorator.java
@@ -5,15 +5,13 @@ import models.enums.Size;
 
 public abstract class CondimentDecorator implements Beverage {
     protected final Beverage beverage;
-    protected Size size;
 
     CondimentDecorator(final Beverage beverage) {
         this.beverage = beverage;
-        this.size = beverage.getSize();
     }
 
     @Override
     public Size getSize() {
-        return size;
+        return beverage.getSize();
     }
 }
